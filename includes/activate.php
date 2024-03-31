946f0881180bc402f362b9e35b530f868f03d537
Mohamed ElHamzawy <mohamed.elhamzawy.backend@gmail.com>
2024-03-31 13:41:20 +0200

    fix activation hook errors

--- includes/activate.php
+++ includes/activate.php
@@ -1,45 +1,42 @@
 <?php
 function rest_api_launch_activation()
 {
-    if (!current_user_can('activate_plugins')) {
-        return;
-    }
-    require ABSPATH . 'wp-admin/includes/upgrade.php';
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
     global $wpdb;
-    dbDelta("CREATE TABLE" . $wpdb->prefix . "prperty(
+    dbDelta("CREATE TABLE " . $wpdb->prefix . "alkamal_property (
         id BIGINT PRIMARY KEY AUTO_INCREMENT,
         title TEXT NOT NULL,
         address LONGTEXT NOT NULL,
         image BIGINT NOT NULL,
         area VARCHAR(25) NOT NULL
     )");
-    dbDelta("CREATE TABLE" . $wpdb->prefix . "contract(
+    dbDelta("CREATE TABLE " . $wpdb->prefix . "alkamal_contract (
         id BIGINT PRIMARY KEY AUTO_INCREMENT,
         propertyId BIGINT NOT NULL,
-        from DATETIME NOT NULL,
-        to DATETIME NOT NULL,
+        contract_from DATETIME NOT NULL,
+        contract_to DATETIME NOT NULL,
         rent FLOAT,
         deposit FLOAT,
         paperContract TEXT,
         eContract TEXT,
         insurance FLOAT,
         commission FLOAT,
-        incrementalRatio FLOAT,
+        incrementalRatio FLOAT
     )");
-    dbDelta("CREATE TABLE" . $wpdb->prefix . "electric(
+    dbDelta("CREATE TABLE " . $wpdb->prefix . "alkamal_electric (
         id BIGINT PRIMARY KEY AUTO_INCREMENT,
         propertyId BIGINT NOT NULL,
         electricSource TEXT NOT NULL,
         electricMeter TEXT NOT NULL,
         meterNumber TEXT NOT NULL,
-        electricReceipt TEXT NOT NULL,
+        electricReceipt TEXT NOT NULL
     )");
-    dbDelta("CREATE TABLE" . $wpdb->prefix . "internet(
+    dbDelta("CREATE TABLE " . $wpdb->prefix . "alkamal_internet (
         id BIGINT PRIMARY KEY AUTO_INCREMENT,
         propertyId BIGINT NOT NULL,
         internetCompany TEXT NOT NULL,
         internetNumber TEXT NOT NULL,
         internetFrom DATETIME NOT NULL,
-        internetTo DATETIME NOT NULL,
+        internetTo DATETIME NOT NULL
     )");
 }
