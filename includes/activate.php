c7ce126227a0a0752a7afafef02867e1d4cf166f
Mohamed ElHamzawy <mohamed.elhamzawy.backend@gmail.com>
2024-04-09 12:56:35 +0200

    update database tables

--- includes/activate.php
+++ includes/activate.php
@@ -3,40 +3,46 @@ function rest_api_launch_activation()
 {
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
     global $wpdb;
+
     dbDelta("CREATE TABLE " . $wpdb->prefix . "alkamal_property (
         id BIGINT PRIMARY KEY AUTO_INCREMENT,
-        title TEXT NOT NULL,
-        address LONGTEXT NOT NULL,
-        image BIGINT NOT NULL,
-        area VARCHAR(25) NOT NULL
-    )");
-    dbDelta("CREATE TABLE " . $wpdb->prefix . "alkamal_contract (
-        id BIGINT PRIMARY KEY AUTO_INCREMENT,
-        propertyId BIGINT NOT NULL,
-        contract_from DATETIME NOT NULL,
-        contract_to DATETIME NOT NULL,
-        rent FLOAT,
-        deposit FLOAT,
-        paperContract TEXT,
-        eContract TEXT,
+        propertyName TEXT,
+        address TEXT NOT NULL,
+        area VARCHAR(25) NOT NULL,
+        status VARCHAR(25) NOT NULL,
+        startAt DATETIME,
+        endAt DATETIME,
+        images LONGTEXT NOT NULL,
+        rentValue FLOAT,
+        depositValue FLOAT,
+        meterPrice FLOAT,
+        paperContractNumber TEXT,
+        digitlyContractNumber TEXT,
         insurance FLOAT,
         commission FLOAT,
-        incrementalRatio FLOAT
+        annualIncrease FLOAT
     )");
-    dbDelta("CREATE TABLE " . $wpdb->prefix . "alkamal_electric (
+    dbDelta("CREATE TABLE " . $wpdb->prefix . "alkamal_electricity (
         id BIGINT PRIMARY KEY AUTO_INCREMENT,
         propertyId BIGINT NOT NULL,
-        electricSource TEXT NOT NULL,
-        electricMeter TEXT NOT NULL,
-        meterNumber TEXT NOT NULL,
-        electricReceipt TEXT NOT NULL
+        FOREIGN KEY (propertyId) REFERENCES " . $wpdb->prefix . "alkamal_property(id) ON DELETE CASCADE,
+        sourceOfElectricity TEXT,
+        electricCounterNumber TEXT,
+        accountNumber TEXT,
+        bill LONGTEXT,
+        receipt LONGTEXT,
+        bond LONGTEXT
     )");
     dbDelta("CREATE TABLE " . $wpdb->prefix . "alkamal_internet (
         id BIGINT PRIMARY KEY AUTO_INCREMENT,
         propertyId BIGINT NOT NULL,
-        internetCompany TEXT NOT NULL,
-        internetNumber TEXT NOT NULL,
-        internetFrom DATETIME NOT NULL,
-        internetTo DATETIME NOT NULL
+        FOREIGN KEY (propertyId) REFERENCES " . $wpdb->prefix . "alkamal_property(id) ON DELETE CASCADE,
+        internetCompany TEXT,
+        startAt DATETIME,
+        endAt DATETIME,
+        transactionNumber TEXT,
+        bill LONGTEXT,
+        receipt LONGTEXT,
+        bond LONGTEXT
     )");
 }
