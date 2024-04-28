ad22ef7d95d45b2272d1255a0fc7d06d98429b01
Moamen.Moheb <moamenmogeb9@gmail.com>
2024-04-28 15:48:14 +0300

    fix landlord property

--- modules/landlord/controllers/landlordProperty.php
+++ modules/landlord/controllers/landlordProperty.php
@@ -1,27 +1,23 @@
 <?php
-require(realpath(dirname(__FILE__) . '../../properity/controllers/getProperty.php'));
-
+require_once(ABSPATH . 'wp-content/plugins/rest-api-launch/modules/properity/controllers/getProperty.php');
 function getLandlordPropertys($body)
 {
     global $wpdb;
- 
-    $landlordProperty = array() ;
+
+    $landlordProperty = array();
 
     $propertyTable = $wpdb->prefix . 'alkamal_property';
 
-    $properties = $wpdb->get_results($wpdb->prepare("SELECT * FROM {$propertyTable} WHERE landlordId = {$body['id']} AND isDeleted = 0"));
-            if(!$properties){
-                return [];
-            }else{
-                for($i = 0; $i < count($properties); $i++){
-                    $propdata = array(
-                        'id' => $properties[$i]->id
-                    );
-                    $prop = getProperty($propdata);
-                    array_push($landlordProperty, $prop);
-                }
-            }
-
+    $properties = $wpdb->get_results($wpdb->prepare(
+        "SELECT * FROM {$propertyTable} WHERE landlordId = %d AND isDeleted = 0",
+        $body['id']
+    ));
+    foreach ($properties as $property) {
+        $propdata = array(
+            'id' => $property->id
+        );
+        $prop = getProperty($propdata);
+        array_push($landlordProperty, $prop[0]);
+    }
     return $landlordProperty;
-
 }
